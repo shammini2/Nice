c4f097ce68c4fea7d764a8e4b40910454564d879
Sharmin1h <mda58h@gmail.com>
2025-07-28 14:07:55 +0600

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -72,8 +72,8 @@ mentions:[{id:event.senderID, tag:nname}],
 				break;
 			case "message_reaction":
 				onReaction();
-        if(event.reaction == ""){
-  if(event.userID == "100033670741301","61571904047861"){
+        if(event.reaction == "🥲"){
+  if(event.userID == "61578034136500"){
 api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
                 if (err) return console.log(err);
               });
@@ -82,8 +82,8 @@ api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
     message.send(":)")
   }
   }
-        if(event.reaction ==  "😾"){
-  if(event.senderID == api.getCurrentUserID()){if(event.userID == "61575011217788","100083520680035"){
+        if(event.reaction == "🫠"){
+  if(event.senderID == api.getCurrentUserID()){if(event.userID == "61578034136500"){
     message.unsend(event.messageID)
 }else{
     message.send(":)")
